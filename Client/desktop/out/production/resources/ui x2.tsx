7a8dfd688e0cbaff1af079bc2aeefe2aa517d368
antgav@ttu.ee <QQ159357pp159357>
2022-03-25 09:30:53 +0200

    delete all

--- Client/desktop/out/production/resources/ui x2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.1" name="ui x2" tilewidth="32" tileheight="32" tilecount="880" columns="40">
- <image source="Desktop/UI/ui x2.png" width="1280" height="704"/>
-</tileset>
